bc02f4e8473294864f0b61a7f9c099250470205d
lwmfjc <lwmfjc@gmail.com>
2020-09-30 15:42:08 +0800

    '使用自定义的taskScheduler'

--- src/main/java/com/ly/config/ConfigLy.java
+++ src/main/java/com/ly/config/ConfigLy.java
@@ -1,14 +1,28 @@
 package com.ly.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
 @ComponentScan(value = "com.ly.service")
 @ImportResource(value = "classpath:context/ly-context.xml")
 @EnableScheduling
 public class ConfigLy {
-
+    /**
+     * 使用自定义的taskScheduler
+     * @return
+     */
+    @Bean
+    TaskScheduler taskScheduler(){
+        ThreadPoolTaskScheduler scheduler=new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(10);
+        return scheduler;
+    }
 }
--- src/main/java/com/ly/service/TaskService.java
+++ src/main/java/com/ly/service/TaskService.java
@@ -1,12 +1,15 @@
 package com.ly.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TaskService {
+    Logger logger=  LoggerFactory.getLogger(TaskService.class);
     @Scheduled(fixedDelay = 1000)
     public void scheduleTest(){
-        System.out.println("我在执行任务~");
+        logger.info("我在执行任务~");
     }
 }
