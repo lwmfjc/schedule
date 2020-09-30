0ce125f7d6320792667fb9ce6bb6ecb57159d671
lwmfjc <lwmfjc@gmail.com>
2020-09-30 15:31:04 +0800

    '使用注解来使用spring调度任务'

--- src/main/java/com/ly/config/ConfigLy.java
+++ src/main/java/com/ly/config/ConfigLy.java
@@ -3,10 +3,12 @@ package com.ly.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @ComponentScan(value = "com.ly.service")
 @ImportResource(value = "classpath:context/ly-context.xml")
+@EnableScheduling
 public class ConfigLy {
 
 }
--- src/main/java/com/ly/service/TaskService.java
+++ src/main/java/com/ly/service/TaskService.java
@@ -1,9 +1,11 @@
 package com.ly.service;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TaskService {
+    @Scheduled(fixedDelay = 1000)
     public void scheduleTest(){
         System.out.println("我在执行任务~");
     }
